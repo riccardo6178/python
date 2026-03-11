524342d27e31d81972c7aae1d5f9391bb10dde6a
riccardo6178 <rrizzo@stevejobs.academy>
2026-03-11 17:38:21 +0100

    Aggiunti nuovi file

---
+++ oop.py
@@ -0,0 +1,14 @@
+class Cerchio:
+        def __init__(self, r):
+            self.raggio=r
+
+        def area(self):
+            return self.raggio*self.raggio*3.14
+
+        def perimetro(self):
+            return 2*3.14*self.raggio
+c=Cerchio(12)
+print(c.area())
+print(f"Raggio: {c.raggio}")
+print(f"perimetro: {c.perimetro()}")
+print(c)
